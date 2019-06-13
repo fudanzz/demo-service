521def6cb3de47c1ad1df2f5d67e30201a331e95
Zhao <zhongz@88e9fe7b82be.ant.amazon.com>
2019-06-13 17:23:25 +0800

    update code

--- functions/helloworld.js
+++ functions/helloworld.js
@@ -4,7 +4,7 @@ module.exports.handler = async (event) => {
   return {
     statusCode: 200,
     body: JSON.stringify({
-      message: 'Hello World'
+      message: 'Hello World!'
     }, null, 2),
   };
 
